2e150be2d64610ac835e606b14bcc6aa33914ecf
Vitaliy Bobrov <bobrov1989@gmail.com>
2016-10-27 09:05:09 +0200

    fix class checks

--- angular-hot-loader.js
+++ angular-hot-loader.js
@@ -16,6 +16,12 @@ var HotAngular = function(settings) {
   this.name;
   this.bootstrapElement;
 
+  var toString = Function.prototype.toString;
+
+  function fnBody(fn) {
+    return toString.call(fn).replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'');
+  }
+
   function isClass(fn) {
     return (typeof fn === 'function' &&
             (/^class\s/.test(toString.call(fn)) ||
